cb1165038d74405644812f131bc36708dd969317
Matt Brogan <broganmatt11@gmail.com>
2020-06-27 09:56:17 -0400

    Todo list completed

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -9,7 +10,48 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            /* Create two Lists of type int.
+             * Name one List "evens"
+             * Name the other List "odds"
+             */
+            List<int> odd = new List<int>();
+            List<int> evens = new List<int>();
+            /* Using either a foreach or for loop,
+             * nest an if statement to check to see
+             *  if a number is even or odd.
+             * Then add those numbers to either the evens List
+             * or the odds List
+             */
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evens.Add(i);
+                }
+                else
+                {
+                    odd.Add(i);
+                }
+            }
+            Console.WriteLine("Watch me count only even numbers");
+            foreach (int num in evens)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Now I'll count all the odd numbers");
+            foreach (int num in odd)
+            {
+                Console.WriteLine(num);
+            }
 
+
+            /* Now using foeach or for loops,
+             * display each List of even and odd numbers
+             *
+             * Try to be creative in your display
+            //TODO:
+            // Create an int Array and populate numbers 1-10
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
